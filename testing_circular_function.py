78f38ea237922236535e6b1bf27ad61ae572a5ad
Kaliroe <kmpappa2@illinois.edu>
2017-07-07 09:17:44 -0500

    updated file

--- testing_circular_function.py
+++ testing_circular_function.py
@@ -9,31 +9,59 @@ Created on Wed Jun 28 20:47:47 2017
 import random as rd
 import numpy as np
 import matplotlib.pyplot as plt
-import constants as cn
+from binary_tools.utils.constants import *
+from binary_tools.utils import kicks
+from binary_tools.utils.Keplers_laws import *
 from scipy.stats import maxwell
-rd.seed(10)
-from functions import rand_phi, rand_theta, rand_velocity
-from circular_function import circular_conditions
 
-test_c = 100
-test_M1 = 5.5
-test_M2 = 55
-test_Ai = 133
-test_Mns = 1.4
+__author__ = "Kaliroe Pappas"
+__credits__ = ["Kaliroe Pappas"]
+__license__ = "GPL"
+__version__ = "3.0"
+__maintainer__ = "Kaliroe Pappas"
+__email__ = "kmpappa2@illinois.edu"
 
-"""creating an array of eccentricity and semi-major axis values""" 
-  
-testing_function = np.zeros([100,2])
 
-for i in range(len(testing_function)):
-    testing_function[i][0] = circular_conditions(test_Ai, test_M1, test_M2, test_Mns, rand_theta(),rand_phi(),rand_velocity(test_c))[1]
-    testing_function[i][1] = circular_conditions(test_Ai, test_M1, test_M2, test_Mns, rand_theta(),rand_phi(),rand_velocity(test_c))[2]
+"""A function that tests the post_explosion_params_circular function""" 
 
-"""changing the semi-major axis to period values in days"""
+def testing_circular_function(test_sigma = 15, test_M1 = 5.5, test_M2 = 55, test_Ai = 133, test_Mns = 1.4, seed="Flay",sample_velocity = 30):
+    """Test that the graph of the eccentricity vs the period looks correct
+    Arguments:
+        - test_sigma: a sample sigma for the rand_velocity function
+        - test_M1 = solar mass of the first mass pre-explosion
+        - test_M2 = solar mass of the second mass
+        - test_Ai = the initial maximum separation of the two masses
+            pre-explosion
+        - test_Mns = solar mass of the first mass post-explosion
+        - seed: the seed used for the random number generator
+        - sample_velocity = a constant velocity over which a line is
+            drawn on the graph
+    Returns: 'Compare graph to paper'
+    """  
+    rd.seed(seed)
+    testing_function = np.zeros([1000,2])
+    constant_velocity = np.zeros([1000,2])
 
-for k in range(len(testing_function)):
-    testing_function[k][1] = (np.sqrt(((testing_function[k][1])**3)*4*(np.pi**2)/(cn.cgrav*(test_M1+test_Mns)*cn.Msun)))/(3600*24)
+    for i in range(len(testing_function)):  
+        testing_function[i][0] = kicks.post_explosion_params_circular(test_Ai, test_M1, test_M2, test_Mns, kicks.rand_theta(),kicks.rand_phi(),kicks.rand_velocity(test_sigma))[0]
+        testing_function[i][1] = kicks.post_explosion_params_circular(test_Ai, test_M1, test_M2, test_Mns, kicks.rand_theta(),kicks.rand_phi(),kicks.rand_velocity(test_sigma))[1]
     
-plt.plot(testing_function[:,0], testing_function[:,1], "o")
-plt.xlim([0.0726,0.0728])
+    for j in range(len(constant_velocity)):
+        constant_velocity[j][0] = kicks.post_explosion_params_circular(test_Ai, test_M1, test_M2, test_Mns, kicks.rand_theta(),kicks.rand_phi(),sample_velocity)[0]
+        constant_velocity[j][1] = kicks.post_explosion_params_circular(test_Ai, test_M1, test_M2, test_Mns, kicks.rand_theta(),kicks.rand_phi(),sample_velocity)[1]
+    
+    """changing the semi-major axis to period values in days"""
+    
+    for k in range(len(testing_function)):
+        testing_function[k][0] = keplers_third_law(testing_function[k][0],test_M2,test_Mns)
+        constant_velocity[k][0] = keplers_third_law(constant_velocity[k][0],test_M2,test_Mns)
+    
+    plt.plot(testing_function[:,0], testing_function[:,1], "o")
+    plt.plot(constant_velocity[:,0], constant_velocity[:,1], 'k-')
+    plt.show()
+    plt.close()
+    
+    rd.seed()
+    
+    return "Compare graph to paper"
     
