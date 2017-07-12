67af09d19d52e6571495bef14469ad549d1e6282
Kaliroe <kmpappa2@illinois.edu>
2017-07-12 12:32:38 -0500

    updated

--- utils/Keplers_laws.py
+++ utils/Keplers_laws.py
@@ -22,14 +22,14 @@ def keplers_third_law(semi_major_axis, mass_1, mass_2):
     """Takes accepted arguments and returns the period
     of the two-mass system
     Arguments:
-        semi_major_axis: largest seperation of the masses in units of km
+        semi_major_axis: largest seperation of the masses in units of solar radii
         mass_1: mass of the first mass in solar masses
         mass_2: mass of the second mass in solar masses
     Returns the period of the system in units of days"""
     
     mass_1 = mass_1*Msun
     mass_2 = mass_2*Msun
-    semi_major_axis = semi_major_axis*1e5
+    semi_major_axis = semi_major_axis*Rsun
     period = np.sqrt(4*np.pi**2*(semi_major_axis**3)/(cgrav*(mass_1+mass_2)))/(3600*24)
     return period
     
