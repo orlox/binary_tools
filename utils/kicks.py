88ec1e48797aa7dc8f8474d95bf4d2549512b670
Kaliroe <kmpappa2@illinois.edu>
2017-07-17 18:48:14 -0500

    minor change

--- utils/kicks.py
+++ utils/kicks.py
@@ -152,7 +152,7 @@ def rand_separation(e, Ai):
     return separation
 
 
-def post_explosion_params_general(Ai, M1, M2, Mns, phi, Vk, true_anomaly, e):
+def post_explosion_params_general(Ai, M1, M2, Mns, theta, Vk, true_anomaly, e):
     """Computes the post explosion orbital parameters of a binary system,
     assuming the pre-explosion system is in a circular orbit.
     Calculation follows Kalogera (1996), ApJ, 471, 352
@@ -162,7 +162,7 @@ def post_explosion_params_general(Ai, M1, M2, Mns, phi, Vk, true_anomaly, e):
         - M1: initial mass of the exploding star in Msun
         - M2: mass of the companion star in Msun
         - Mns: final mass of the exploding star in Msun
-        - phi: azimuthal angle of the kick direction
+        - theta: azimuthal angle of the kick direction
         - Vk: kick velocity, in km/s
         - e: eccentricity of the orbit
         - u: the true anomaly in radians
@@ -183,12 +183,12 @@ def post_explosion_params_general(Ai, M1, M2, Mns, phi, Vk, true_anomaly, e):
     
     V_squared = (Vk**2)*np.cos(true_anomaly)**2 + 2*Vk*np.cos(true_anomaly)*V_theta\
     + V_theta**2 + V_radius**2 + (Vk**2)*np.sin(true_anomaly)**2\
-    + 2*Vk*np.sin(true_anomaly)*np.cos(phi)*V_radius
+    + 2*Vk*np.sin(true_anomaly)*np.cos(theta)*V_radius
     
     Af = cgrav*(Mns + M2)*(2*cgrav*(Mns + M2)/Ai - V_squared)**(-1)
     
     e_final = np.sqrt(1 - ((Vk*np.cos(true_anomaly))**2 + 2*Vk*V_theta*np.cos(true_anomaly)\
-    + V_theta**2 + (Vk*np.sin(true_anomaly)*np.cos(phi))**2)*Ai**2/(cgrav*(Mns + M2)*Af))
+    + V_theta**2 + (Vk*np.sin(true_anomaly)*np.cos(theta))**2)*Ai**2/(cgrav*(Mns + M2)*Af))
     
     bound = True
     if e_final > 1:
